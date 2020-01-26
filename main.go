bae0021bc694996c000ac5e711824849a6618e2a
Behruz Valiev <behruzvaliev@gmail.com>
2020-01-26 08:41:08 +0500

    first push

--- main.go
+++ main.go
@@ -29,148 +29,70 @@ func CreateQueue() *Queue {
 	return &Queue{}
 }
 
-// AddQueue ...
-func (p *Queue) AddQueue(name, priority string) {
-	s := &Node{
-		name:     name,
-		priority: priority,
-	}
-
-	if p.head == nil {
-		p.head = s
-	} else {
-		currentNode := p.tail
-		currentNode.next = s
-		s.previous = p.tail
-	}
-	p.tail = s
-	p.length++
-
-}
-
+// showAllQueues show all
 func (p *Queue) showAllQueues() error {
 	currentNode := p.head
 	if currentNode == nil {
 		fmt.Println("Queue is empty")
 		return nil
 	}
-	fmt.Printf("Z%+v\n", *currentNode)
+	fmt.Printf("%+v\n", *currentNode)
 	for currentNode.next != nil {
 		currentNode = currentNode.next
-		fmt.Printf("Q%+v\n", *currentNode)
+		fmt.Printf("%+v\n", *currentNode)
 	}
 	return nil
 }
 
-func (p *Queue) startQueue() *Node {
-	p.nowInQueue = p.head
-	return p.nowInQueue
-}
-
-func (p *Queue) nextQueue() *Node {
-	p.nowInQueue = p.nowInQueue.next
-	return p.nowInQueue
-}
-
-func (p *Queue) previousQueue() *Node {
-	p.nowInQueue = p.nowInQueue.previous
-	return p.nowInQueue
-}
-
 // First ...
-func (p *Queue) First() {
+func (p *Queue) First() string {
 	p.nowInQueue = p.head
-	fmt.Println("First element: ", p.nowInQueue.name)
+	return p.nowInQueue.name
 }
 
 // Last ...
-func (p *Queue) Last() {
+func (p *Queue) Last() string {
 	p.nowInQueue = p.tail
-	fmt.Println("Last element: ", p.nowInQueue.name)
-}
-
-func main() {
-	myQueue := CreateQueue()
-	// fmt.Println("Created queue")
-	// fmt.Println()
-	myQueue.Insert()
-	// fmt.Print("Adding clients to queue...\n\n")
-	// myQueue.showAllQueues()
-	// myQueue.insertVip("Tyler", "vip")
-	myQueue.AddQueue("Marla", "simple")
-	myQueue.AddQueue("Bob", "simple")
-	myQueue.AddQueue("Cornelius", "simple")
-	myQueue.AddQueue("Dod", "simple")
-	// myQueue.insertVip("Dan", "vip")
-
-	// need show positon in queue
-
-	// fmt.Println("Show len of queue: ", myQueue.length)
-	// fmt.Println()
-
-	// fmt.Println("Showing all clients in queue...")
-	// myQueue.showAllQueues()
-	// fmt.Println()
-
-	// myQueue.startQueue()
-	// fmt.Printf("Now in queue: %s priority %s\n", myQueue.nowInQueue.name, myQueue.nowInQueue.priority)
-	// fmt.Println()
-
-	// myQueue.nextQueue()
-	// fmt.Println("Call next client...")
-	// fmt.Printf("Now in queue: %s priority %s\n", myQueue.nowInQueue.name, myQueue.nowInQueue.priority)
-	// fmt.Println()
-
-	// myQueue.previousQueue()
-	// fmt.Println("Back to previous client...")
-	// fmt.Printf("Now in queue: %s priority %s\n", myQueue.nowInQueue.name, myQueue.nowInQueue.priority)
-	// fmt.Println()
-
-	// fmt.Println()
-	// myQueue.First()
-	// myQueue.Last()
-
-	// fmt.Println()
-	// myQueue.Remove()
-	// fmt.Println()
-	// myQueue.First()
-	// myQueue.Last()
-	// myQueue.showAllQueues()
+	return p.nowInQueue.name
 }
 
 // Remove ...
-func (p *Queue) Remove() {
+func (p *Queue) Remove() error {
 	if p.length == 0 {
 		fmt.Printf("Queue is empty")
+		return nil
 	}
 
-	var previousClient *Node
-	currentNode := p.head
-
-	previousClient = currentNode
-	currentNode = currentNode.next
-	previousClient.next = currentNode.next
 	p.length--
 
+	if p.head.next == nil {
+		p.head = nil
+		return nil
+	}
+
+	currentNode := p.head
+	previous := p.head
+	currentNode = currentNode.next
+	previous.next = currentNode.next
 	p.head = currentNode
 	p.head.previous = nil
+	return nil
 }
 
 // Insert ...
 func (p *Queue) Insert(d ...Node) bool {
 	newNode := &d[0]
+	p.length++
 	if newNode.priority == "vip" {
 		if p.head.priority == "vip" {
 			newNode.next = p.head.next
 			p.head.next = newNode
 			newNode.previous = p.head
-			p.length++
 			return true
 		}
 		newNode.next = p.head
 		p.head.previous = newNode
 		p.head = newNode
-		p.length++
 		return true
 
 	}
@@ -184,6 +106,8 @@ func (p *Queue) Insert(d ...Node) bool {
 	}
 
 	p.tail = newNode
-	p.length++
 	return true
 }
+
+// Main ...
+func main() {}
--- main_test.go
+++ main_test.go
@@ -1,60 +1,108 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-// func Test_empty_list(t *testing.T) {
-// 	q := CreateQueue()
-// 	if q.length != 0 {
-// 		t.Error("new list length must be zero, got: ", q.length)
-// 	}
-// }
-
-// func Test_list_with_one_item(t *testing.T) {
-// 	q := CreateQueue()
-// 	q.AddQueue("Bob", "simple")
-// 	if q.length != 1 {
-// 		t.Error("after adding one element size must be 1, got: ", q.length)
-// 	}
-// }
-
-// func Test_list_with_multiple_items(t *testing.T) {
-// 	q := CreateQueue()
-// 	q.AddQueue("Tyler", "smple")
-// 	q.AddQueue("Marla", "smple")
-// 	q.AddQueue("Cornelius", "smple")
-// 	if q.length != 3 {
-// 		t.Error("after adding 3 elements size must be 3, got: ", q.length)
-// 	}
-// }
-
-func TestVipClient(t *testing.T) {
+func Test_empty_list(t *testing.T) {
 	q := CreateQueue()
-	a := &Node{
-		name:     "Marla",
-		priority: "simple",
+	if q.length != 0 {
+		t.Error("new list length must be zero, got: ", q.length)
 	}
-	q.Insert(*a)
-	q.AddQueue("Roger", "simple")
-	q.AddQueue("Roger2", "simple")
-	ab := &Node{
+}
+
+func Test_remove_client(t *testing.T) {
+	q := CreateQueue()
+
+	q.Insert(Node{
+		name:     "Bob",
+		priority: "simple",
+	})
+	q.Remove()
+	if q.length != 0 {
+		t.Error("remove function don't work")
+	}
+}
+
+func Test_list_with_one_item(t *testing.T) {
+	q := CreateQueue()
+	sec := &Node{
 		name:     "Bob",
 		priority: "simple",
 	}
-	q.Insert(*ab)
-	ac := &Node{
+	q.Insert(*sec)
+	if q.length != 1 {
+		t.Error("after adding one element size must be 1, got: ", q.length)
+	}
+}
+
+func Test_list_with_multiple_items(t *testing.T) {
+	q := CreateQueue()
+	fir := &Node{
+		name:     "Marla",
+		priority: "simple",
+	}
+	q.Insert(*fir)
+	sec := &Node{
+		name:     "Bob",
+		priority: "simple",
+	}
+	q.Insert(*sec)
+	thi := &Node{
 		name:     "Tyler",
 		priority: "vip",
 	}
-	q.Insert(*ac)
-	ad := &Node{
-		name:     "Cornelius",
+	q.Insert(*thi)
+	if q.length != 3 {
+		t.Error("after adding 3 elements size must be 3, got: ", q.length)
+	}
+}
+
+func TestVipClient(t *testing.T) {
+	q := CreateQueue()
+	Marla := &Node{
+		name:     "Marla",
+		priority: "simple",
+	}
+	q.Insert(*Marla)
+	Bob := &Node{
+		name:     "Bob",
+		priority: "simple",
+	}
+	q.Insert(*Bob)
+	Tyler := &Node{
+		name:     "Tyler",
 		priority: "vip",
 	}
-	q.Insert(*ad)
-	q.showAllQueues()
+	q.Insert(*Tyler)
+	Tyler2 := &Node{
+		name:     "Tyler2",
+		priority: "vip",
+	}
+	q.Insert(*Tyler2)
+	Tyler3 := &Node{
+		name:     "Tyler3",
+		priority: "vip",
+	}
+	q.Insert(*Tyler3)
+	// q.showAllQueues()
 	if q.head.priority != "vip" {
 		t.Error("vip client not inserted to start of queue")
 	}
-	// q.First() // show first element
-	// log.Println("length of queue: ", q.length)
+}
+
+func TestFirst(t *testing.T) {
+	q := CreateQueue()
+	q.Insert(Node{name: "Bob", priority: "simple"})
+	if q.First() != "Bob" {
+		t.Error("First element name must be Bob, got: ", q.nowInQueue.name)
+	}
+}
+
+func TestLast(t *testing.T) {
+	q := CreateQueue()
+	q.Insert(Node{name: "Bob", priority: "simple"})
+	if q.Last() != "Bob" {
+		t.Error("Last element name must be Bob, got: ", q.nowInQueue.name)
+	}
 }
